e9754ce71a0b089aef5c6d5406a2ae853721dd77
KueiKuei <r03228001@ntu.edu.tw>
2017-02-05 21:13:29 +0800

    make it better

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -4,6 +4,7 @@
 var REQUEST_URL = 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json';
 var data = '';
 var str = '';
+
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET',REQUEST_URL);
@@ -44,7 +45,7 @@ xhr.onreadystatechange=function(){
 
 //put data into HTML
 var dataList = function dataList() {
-  console.log(data);
+  // console.log(data);
   data.map(function (key, index) {
     //ES6 把前端變數丟入
 
